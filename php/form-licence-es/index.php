068c45e907616a0c4a8fb7bbf5ba2cae9536250a
javtr <javtriana.z@gmail.com>
2022-03-30 11:22:32 -0500

    form licence

--- php/form-licence-es/index.php
+++ php/form-licence-es/index.php
@@ -39,9 +39,8 @@ if (!empty($_SESSION['_contact_form_success'])) {
 <body>
 
     <!-- ====================== form =========================== -->
-    <div class="container">
+    <!-- <div class="container">
     <div class="general">
-
     <div class="logo">
             <a href="../../index.html"><img src="../../img/logo_azul.svg" alt=""></a>
         </div>
@@ -54,7 +53,6 @@ if (!empty($_SESSION['_contact_form_success'])) {
                 <?php
                     if (!empty($success)) {
                             ?>
-                        <!-- <div class="alert alert-success">Your message was sent successfully!</div> -->
                         <div class="sucess">
                             <a class="sucess-element">Tu mensaje ha sido entregado! <br> pronto enviaremos a tu correo tu software OTMtrading</a>
                         </div>
@@ -66,7 +64,6 @@ if (!empty($_SESSION['_contact_form_success'])) {
                     if (!empty($error)) {
                         ?>
                         <div class="error">
-                            <!-- <a class="error-element">Your message could not be sent. <br> Por favor envier la informacion solicitada a contact@otmtrading.com </a> -->
                             <div><?= $error ?></div>
                             <a class="error-element">Tu mensaje no ha podido ser entragado <br> Si el error persiste contacta a contact@otmtrading.com </a>
                         </div>                        
@@ -120,16 +117,73 @@ if (!empty($_SESSION['_contact_form_success'])) {
 
                         <button class="btn btn-primary btn-block btn">Send Message</button>
 
-                        <!-- <button class="btn btn-primary btn-bloc">
-                        <a href="" >Send</a>
-                        </button> -->
 
                     </form>
 
                 </div>
             </div>
         </div>
+    </div> -->
+
+<!-- -------------------------------------------- -->
+
+<section class="licence">
+    <div class="licence-container">
+        <div class="licence-logo">
+            <img src="../../img/logo_azul.svg" alt="">
+        </div>
+        <div class="licence-card">
+            <div class="licence-header">¡No cierres esta ventana hasta completar tu proceso!</div>
+            <div class="licence-title">Gracias por adquirir OTM Software</div>
+            <div class="licence-subtitle">Sigue los siguientes pasos para obtener OTM</div>
+            <div class="licence-numeral">1. Obten el Machine ID de tu ninjatrader</div>
+            <div class="licence-img">
+                <img src="../../img/id.jpg" alt="" srcset="">
+            </div>
+            <div class="licence-pieimagen">El código Machine ID es generado por NinjaTrader basado en el hardware y software principal de sistema</div>
+            <div class="licence-numeral2">2. Después de enviar este formulario, crearemos la licencia para tu equipo.</div>
+
+
+
+
+
+            <div class="licence-form--input">
+                <h2>Nombre <span>*</span></h2>
+                <input type="text" name="name" id="name" class="licence-form--box" placeholder="John Doe">
+            </div>
+
+            <div class="licence-form--input">
+            <h2>Email <span>*</span> </h2>
+                <input type="email" name="email" id="email" class="licence-form--box" placeholder="name@example.com">
+            </div>
+
+            <div class="licence-form--input">
+            <h2> Machine ID<span>*</span> </h2>
+                <input type="text" name="subject" id="subject" class="licence-form--box" placeholder="1234123412341234">
+            </div>
+
+            <div class="licence-form--input">
+            <h2>Mensage</h2>
+                <textarea name="message" id="message" class="licence-form--boxMesagge" rows="6" placeholder="" ></textarea>
+            </div>
+
+            <div class="g-recaptcha cap2" data-sitekey="<?= CONTACTFORM_RECAPTCHA_SITE_KEY ?>"></div>
+
+
+            <button class="btn btn-primary btn-block btn licence-form--send">SEND</button>
+        
+            <div class="licence-text part-a">• Recibirás en tu correo tu software en un plazo de 1 día hábil (8 a 5 PM GMT- 4).</div>
+            <div class="licence-text part-b">• Si tienes cualquier inquietud no dudes en contactarnos:</div>
+            <div class="licence-text part-c">info@otmtrading.com</div>
+            <div class="licence-text part-c">WhatsApp de soporte +573113006826</div>
+        
+        
+        </div>            
     </div>
+</section>
+
+
+
 </div>
 
 
